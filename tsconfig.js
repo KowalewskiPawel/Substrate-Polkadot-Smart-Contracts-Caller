a3eae4a390ffe2916d6916d9e686cdb512e1e858
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-02-18 15:48:58 +0100

    add-linter-config

---
+++ tsconfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "compilerOptions": {
+      "module": "CommonJS",
+      "esModuleInterop": true,
+      "target": "es6",
+      "moduleResolution": "node",
+      "sourceMap": true,
+      "outDir": "dist",
+    },
+    "lib": ["es2015"]
+  }
