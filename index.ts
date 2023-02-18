3239a6cfb3c4c696da4ba26b29e841edd34ff0b9
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-02-18 18:28:05 +0100

    add keyring

--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
-import { ApiPromise, WsProvider } from "@polkadot/api";
+import { ApiPromise, WsProvider, Keyring } from "@polkadot/api";
+import { stringToU8a, u8aToHex } from '@polkadot/util';
+import dotenv from "dotenv";
+dotenv.config();
 
 const wsProvider = new WsProvider("wss://ws.test.azero.dev");
+
+const keyring = new Keyring({ type: 'ed25519' });
+
+const phrase = process.env.PHRASE as string;
 
 const connectToProvider = async () => {
   const api = new ApiPromise({ provider: wsProvider });
@@ -10,3 +17,11 @@ const connectToProvider = async () => {
 };
 
 connectToProvider();
+
+const alice = keyring.addFromUri(phrase);
+
+const message = stringToU8a('this is our message');
+const signature = alice.sign(message);
+const isValid = alice.verify(message, signature, alice.publicKey);
+
+console.log(`The signature ${u8aToHex(signature)}, is ${isValid ? '' : 'in'}valid`);
